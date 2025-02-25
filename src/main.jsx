a7260ef98a898996b691e1a8a9999c01a27371eb
Derek Blank <derekpblank@gmail.com>
2025-02-26 09:33:07 +1000

    Use Router for main component

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import App from "./App";
 import Pink from "./Pink";
 import Orbit from "./Orbit";
+import Equalizer from "./Equal";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* <App /> */}
-    {/* <Pink /> */}
-    <Orbit />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/pink" element={<Pink />} />
+        <Route path="/orbit" element={<Orbit />} />
+        <Route path="/equal" element={<Equalizer />} />
+      </Routes>
+    </Router>
   </React.StrictMode>,
 );
